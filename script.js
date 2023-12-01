7ea72d2a86220bb2b9820dc4fd17fc8ea70feab9
samuelcodess <sifenkwe@stu.cu.edu.ng>
2023-12-01 14:26:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleDropdown(dropdownId) {
+    var dropdownContent = document.getElementById(dropdownId);
+    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+}
